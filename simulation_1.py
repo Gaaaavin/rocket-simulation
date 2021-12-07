afd2481ac0f9d1a122649e0458535261ec6e7101
Gaaaavin <gavinliu_hm@hotmail.com>
2021-12-07 12:17:12 -0500

    update simulation

--- simulation_1.py
+++ simulation_1.py
@@ -1,3 +1,9 @@
+'''
+This script simulates in the setting where
+there is no drag. The simulation is in 1 dimension
+'''
+
+
 import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
@@ -12,28 +18,24 @@ def ode(t, y):
     return output
 
 
-def plot(t, y):
-    fig, ax1 = plt.subplots()
-    color = "tab:red"
+def plot(t, y, acceleration):
+    fig = plt.figure()
+    fig.set_size_inches(10, 15)
+    ax1 = fig.add_subplot(3, 1, 1)
     ax1.set_xlabel("time(s)")
-    ax1.set_ylabel("height", color=color)
-    ax1.plot(t, y[0, :], label="height", color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.set_ylabel("height(m)")
+    ax1.plot(t, y[0, :], 'b', label="height")
 
-    ax2 = ax1.twinx()
-    color = "tab:blue"
-    ax2.set_ylabel("velocity", color=color)
-    ax2.plot(t, y[1, :], label="velocity", color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
+    ax2 = fig.add_subplot(3, 1, 2)
+    ax2.set_ylabel("velocity(m/s)")
+    ax2.plot(t, y[1, :], 'r', label="velocity")
     
-    # ax3 = ax1.twinx()
-    # color = "tab:green"
-    # ax3.set_ylabel("mass", color=color)
-    # ax3.plot(t, y[2, :], label="mass", color=color)
-    # ax3.tick_params(axis='y', labelcolor=color)
+    ax3 = fig.add_subplot(3, 1, 3)
+    ax3.set_ylabel("acceleration(m/s^2)")
+    ax3.plot(t[:-2], acceleration, 'g', label="acceleration")
 
     fig.tight_layout()
-    fig.legend()
+    # fig.legend()
     # fig.show()
     fig.savefig("plot.png")
 
@@ -48,7 +50,10 @@ def main():
     t_eval = np.arange(config["tmax"]+1)
    
     solver = integrate.solve_ivp(ode, (0, config["tmax"]), y0, t_eval=t_eval)
-    plot(solver.t, solver.y)
+    acceleration = np.zeros(config["tmax"]-1)
+    for i in range(config["tmax"]-1):
+        acceleration[i] = solver.y[1, i+1] - solver.y[1, i]
+    plot(solver.t, solver.y, acceleration)
 
 
 
